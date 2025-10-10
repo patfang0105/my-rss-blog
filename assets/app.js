be008ff9e68fbd1873fdff788abb6e61d4d19dbe
patfang0105 <fangxiaoyuan@iefi.org.cn>
2025-10-10 10:02:01 +0800

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,8 @@ const DEFAULT_FEEDS = [
   "https://rhg.com/feed/",
   "https://www.aei.org/feed/",
   "https://www.wto.org/library/rss/latest_news_e.xml",
-  "https://www.foreignaffairs.com/rss.xml"
+  "https://www.foreignaffairs.com/rss.xml",
+  "https://www.piie.com/rss/update.xml"
 ];
 
 const state = {
